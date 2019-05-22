24cf65f0b2f81ac5f98f0edbe609f60e7815a27b
hoannt110 <hoannt.mgc@gmail.com>
2019-05-22 21:57:44 +0700

    test nop bai

---
+++ Session 1/hello.py
@@ -0,0 +1,13 @@
+# name = "John"
+# name = "Clara"
+# print ("Hello", name)
+
+# quit() de quay tro ve script mode.
+# type python de vao interactive mode.
+# name = input("Write your name: ")
+# print ("Boom", name)
+
+# a = int(input("nhap nam sinh:")) convert tat ca nhung gi nhap vao thanh int
+# a = int(a) convert thanh int va gan lai cho no 1 gia tri khac
+#b = 2018 - a
+#print ("Tuoi cua ban tinh den hien tai la", b)
---
+++ Session 1/trochoiconrua.py
@@ -0,0 +1,52 @@
+from turtle import *
+shape("circle")
+speed(10)
+#for i in range(3):
+   # for x in range(4): 
+      #  forward(100)
+      #  left(90)
+   # right(30)
+for i in range(100):
+    circle(40)
+    right(3.6)
+#forward(100)
+#right(90)
+#forward(100)
+#right(90)
+#forward(100)
+#right(90)
+#forward(100)
+#right(90)
+
+#right(30)
+
+#forward(100)
+#right(90)
+#forward(100)
+#right(90)
+#forward(100)
+#right(90)
+#forward(100)
+#right(90)
+
+
+#def move():
+   # forward(100)
+    #left(90)
+    #forward(100)
+    #left(90)
+    #forward(100)
+   # left(90)
+   # forward(100)
+   # left(90)
+   # return
+
+#move()
+
+#i = 1;
+#while(i <= 3):
+  #  left(150)
+  #  move()
+  #  i += 1
+
+mainloop()
